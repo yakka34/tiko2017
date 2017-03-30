a70c6ef75a36168d222eb8bf3499b017e0b15bfb
Z1ni <marktmakinen@gmail.com>
2017-03-30 21:53:59 +0300

    Lisätty käyttäjätiedot kenttiin

--- src/resources/views/account.blade.php
+++ src/resources/views/account.blade.php
@@ -1,7 +1,36 @@
 @extends('home')
 
 @section('panel_content')
-    {{$user}}
+    <form>
+        <div class="form-group">
+            <label for="nameField">Nimi</label>
+            @if (Auth::user()->hasRole('admin'))
+                <input type="text" id="nameField" class="form-control" value="{{$user->name}}">
+            @else
+                <input type="text" id="nameField" class="form-control" value="{{$user->name}}" disabled>
+            @endif
+        </div>
+        <div class="form-group">
+            <label for="studentIdField">Opiskelijanumero</label>
+            @if (Auth::user()->hasRole('admin'))
+                <input type="text" id="studentIdField" class="form-control" value="{{$user->opnro}}">
+            @else
+                <input type="text" id="studentIdField" class="form-control" value="{{$user->opnro}}" disabled>
+            @endif
+        </div>
+        <div class="form-group">
+            <label for="majorField">Pääaine</label>
+            @if (Auth::user()->hasRole('admin'))
+                <input type="text" id="majorField" class="form-control" value="{{$user->major}}">
+            @else
+                <input type="text" id="majorField" class="form-control" value="{{$user->major}}" disabled>
+            @endif
+        </div>
+        <div class="form-group">
+            <label for="emailField">Sähköposti</label>
+            <input type="text" id="emailField" class="form-control" value="{{$user->email}}">
+        </div>
+    </form>
     <h1>Roolisi ovat:</h1>
     <ul>
         @foreach($roles as $role)
