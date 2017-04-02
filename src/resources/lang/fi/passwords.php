e2e0e9a6fd20fd2d16edad7239db65791bc58c7d
Z1ni <marktmakinen@gmail.com>
2017-04-02 14:27:43 +0300

    Käännöksiä ja korjauksia kirjautumiseen/rekisteröitymiseen

---
+++ src/resources/lang/fi/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Salasanan pitää olla vähintään kuusi merkkiä ja vastata vahvistuskenttää.',
+    'reset' => 'Salasanasi on nollattu!',
+    'sent' => 'Salasanan nollauslinkki lähetettiin sähköpostiosoitteeseesi.',
+    'token' => 'Salasananvaihtolinkki on virheellinen.',
+    'user' => "Annetulle sähköpostiosoitteelle ei löydy käyttäjää.",
+
+];
