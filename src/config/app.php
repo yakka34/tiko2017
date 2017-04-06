49b7af923aefa6d825e4612aae38397921607430
yakka34 <tv.lehtinen@hotmail.com>
2017-04-06 18:53:09 +0300

    ide-helper asennettu virallisten ohjeiden mukaan

--- src/config/app.php
+++ src/config/app.php
@@ -176,7 +176,10 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+        /*
+         *  Barryyvhd IdeHelper Provider
+         */
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
     ],
 
     /*
